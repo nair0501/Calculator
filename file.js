937cf91d08d6f746af7239ae8a49ff453942b8e0
Naira <nairaaghayan19@gmail.com>
2024-06-10 19:11:46 +0400

    solving number overflowing issue in the input

--- file.js
+++ file.js
@@ -1,17 +1,36 @@
 const display = document.getElementById("display");
+const buttons = document.getElementsByClassName("button");
+
+const MAX_DIGITS = 15; //
 
 function appendToDisplay(input) {
-  display.value += input;
+  if (display.value.length < MAX_DIGITS) {
+    display.value += input;
+    display.scrollLeft = display.scrollWidth;
+  } else {
+    alert("Maximum digit limit reached");
+  }
 }
-
 function clearDisplay() {
   display.value = "";
 }
-
 function calculate() {
   try {
-    display.value = new Function('return ' + display.value)();
+    display.value = math.evaluate(display.value).toString();
   } catch {
     display.value = "Error";
   }
 }
+buttons.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.classList.contains("button")) {
+    const value = target.getAttribute("data-value");
+    if (value) {
+      appendToDisplay(value);
+    } else if (target.id === "clear") {
+      clearDisplay();
+    } else if (target.id === "equals") {
+      calculate();
+    }
+  }
+});
